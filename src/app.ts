672b4b473e82217d32945e6cd59b547112652f57
Tuan Nguyen <tuan.nguyen@tuni.fi>
2023-10-24 22:02:32 +0300

    added requests for getting all authors or one by author id

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express from 'express'
 import { errorLoggingMiddleware } from './middlewares/error.js'
 import { loggingMiddleware } from './middlewares/logging.js'
 import itemRoutes from './routes/itemsRoutes.js'
+import authorsRoutes from './routes/authorsRoutes.js'
 
 const app = express()
 
@@ -12,5 +13,6 @@ app.use(errorLoggingMiddleware)
 
 // Routes
 app.use('/api/items', itemRoutes)
+app.use('/api/authors', authorsRoutes)
 
 export default app
