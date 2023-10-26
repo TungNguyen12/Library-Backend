fd7c156986ce1f9e7b46a08ea1393bc1acc08018
TungNguyen12 <nvtung382@gamil.com>
2023-10-26 23:44:44 +0300

    userSchema for single source of truth, use .partial() for optimize code, new type: UserDTO, UserUpdate

--- src/schemas/usersSchema.ts
+++ src/schemas/usersSchema.ts
@@ -1,29 +1,26 @@
 import { z } from 'zod'
 
-export const userCreateSchema = z.object({
-  body: z.object({
-    firstName: z.string({
-      required_error: 'First name is required',
-    }),
-    lastName: z.string({
-      required_error: 'Last name is required',
-    }),
-    email: z
-      .string({
-        required_error: 'Email is required',
-      })
-      .email('Not a valid email'),
-    phoneNumber: z.string({
-      required_error: 'Phone number is required',
-    }),
+export const userSchema = z.object({
+  firstName: z.string({
+    required_error: 'First name is required',
+  }),
+  lastName: z.string({
+    required_error: 'Last name is required',
+  }),
+  email: z
+    .string({
+      required_error: 'Email is required',
+    })
+    .email('Not a valid email'),
+  phoneNumber: z.string({
+    required_error: 'Phone number is required',
   }),
 })
 
+export const userCreateSchema = z.object({
+  body: userSchema,
+})
+
 export const userUpdateSchema = z.object({
-  body: z.object({
-    firstName: z.string().optional(),
-    lastName: z.string().optional(),
-    email: z.string().optional(),
-    phoneNumber: z.string().optional(),
-  }),
+  body: userSchema.partial(),
 })
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,8 +1,16 @@
-export interface User {
-  id: string
-  firstName: string
-  lastName: string
-  email: string
-  phoneNumber: string
-  role: string
-}
+import type { userSchema } from '../schemas/usersSchema.js'
+import type { z } from 'zod'
+
+// export interface User {
+//   id: string
+//   firstName: string
+//   lastName: string
+//   email: string
+//   phoneNumber: string
+//   role: string
+// }
+
+type UserDTO = z.infer<typeof userSchema>
+
+export type User = UserDTO & { id: string; role: string }
+export type UserUpdate = Partial<User>
