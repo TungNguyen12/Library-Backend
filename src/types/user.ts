acecebf8b03a688429e79720e0a933c090d50145
TungNguyen12 <nvtung382@gamil.com>
2023-10-27 00:01:13 +0300

    remove comment and create UserUpdate type

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,16 +1,7 @@
 import type { userSchema } from '../schemas/usersSchema.js'
 import type { z } from 'zod'
 
-// export interface User {
-//   id: string
-//   firstName: string
-//   lastName: string
-//   email: string
-//   phoneNumber: string
-//   role: string
-// }
-
 type UserDTO = z.infer<typeof userSchema>
 
 export type User = UserDTO & { id: string; role: string }
-export type UserUpdate = Partial<User>
+export type UserUpdate = Omit<Partial<User>, 'id' | 'role'>
