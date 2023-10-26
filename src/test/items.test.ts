5b4c457090c40015d28039393edc5284f1e72623
Phuoc Nguyen <phuoc94@hotmail.fi>
2023-10-26 21:39:16 +0300

    fix tesing edpoint

--- src/test/items.test.ts
+++ src/test/items.test.ts
@@ -5,7 +5,7 @@ import { getAllItems } from '../models/itemModel.js'
 
 describe('GET /api/items', () => {
   test('should respond with status code 200 and all items', async () => {
-    const response = await request(app).get('/api/items')
+    const response = await request(app).get('/api/v1/items')
     expect(response.statusCode).toBe(200)
     expect(response.body).toMatchObject(getAllItems())
   })
@@ -13,13 +13,13 @@ describe('GET /api/items', () => {
 
 describe('GET /api/items/:itemIndex', () => {
   test('should respond with status code 200 and the item at the specified index', async () => {
-    const response = await request(app).get('/api/items/1')
+    const response = await request(app).get('/api/v1/items/1')
     expect(response.statusCode).toBe(200)
     expect(response.body).toMatchObject(getAllItems()[0])
   })
 
   test('should respond with status code 404 and an error message when the item does not exist', async () => {
-    const response = await request(app).get('/api/items/100')
+    const response = await request(app).get('/api/v1/items/100')
     expect(response.statusCode).toBe(404)
     expect(response.body).toMatchObject({
       msg: 'Item not found',
