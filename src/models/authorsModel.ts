3eab201284559f483cc7d5fe94db44597dbe7f31
Phuoc Nguyen <phuoc94@hotmail.fi>
2023-10-27 15:51:41 +0300

    Fix the ID to remain static

--- src/models/authorsModel.ts
+++ src/models/authorsModel.ts
@@ -5,31 +5,31 @@ import { type Author } from '../types/Author.js'
 export class AuthorRepo {
   authors: Author[] = [
     {
-      id: uuidv4(),
+      id: 'ed20b7b5-89c4-4be2-b282-e65118b158f1',
       firstName: 'Leo',
       lastName: 'Tolstoy',
       books: ['Anna Karenina'],
     },
     {
-      id: uuidv4(),
+      id: '76b63dc5-b6d8-431e-8ceb-05f29bca5548',
       firstName: 'William',
       lastName: 'Shakespeare',
       books: ['Romeo and Juliet', 'Hamlet'],
     },
     {
-      id: uuidv4(),
+      id: '57449635-33b5-4ef0-b879-2b0883ac4cbc',
       firstName: 'Jane',
       lastName: 'Austen',
       books: ['Pride And Prejudice'],
     },
     {
-      id: uuidv4(),
+      id: 'd7ac7679-c4f0-4c23-9f93-86fe84be948a',
       firstName: 'George',
       lastName: 'Orwell',
       books: ['Animal Farm'],
     },
     {
-      id: uuidv4(),
+      id: '14cec075-6c3d-4930-ad9e-f0c1ebace929',
       firstName: 'Jules',
       lastName: 'Verne',
       books: ['Journey To The Centre Of The Earth'],
