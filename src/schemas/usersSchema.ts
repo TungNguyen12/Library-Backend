734f198dbfcd6efbbed898abd113b0b6f8995991
TungNguyen12 <nvtung382@gamil.com>
2023-10-27 16:26:49 +0300

    add strict() in userSchema to not allow user add extra field in update input

--- src/schemas/usersSchema.ts
+++ src/schemas/usersSchema.ts
@@ -1,21 +1,23 @@
 import { z } from 'zod'
 
-export const userSchema = z.object({
-  firstName: z.string({
-    required_error: 'First name is required',
-  }),
-  lastName: z.string({
-    required_error: 'Last name is required',
-  }),
-  email: z
-    .string({
-      required_error: 'Email is required',
-    })
-    .email('Not a valid email'),
-  phoneNumber: z.string({
-    required_error: 'Phone number is required',
-  }),
-})
+export const userSchema = z
+  .object({
+    firstName: z.string({
+      required_error: 'First name is required',
+    }),
+    lastName: z.string({
+      required_error: 'Last name is required',
+    }),
+    email: z
+      .string({
+        required_error: 'Email is required',
+      })
+      .email('Not a valid email'),
+    phoneNumber: z.string({
+      required_error: 'Phone number is required',
+    }),
+  })
+  .strict()
 
 export const userCreateSchema = z.object({
   body: userSchema,
