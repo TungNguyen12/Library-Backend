518f934a0ddc2e2f33c6438f06b2a9bafd384b01
TungNguyen12 <nvtung382@gamil.com>
2023-11-06 21:57:35 +0200

    Convert usersServices' functions to mongoose methods

--- src/services/usersServices.ts
+++ src/services/usersServices.ts
@@ -21,6 +21,7 @@ async function createOne(newUser: User): Promise<User | Error | undefined> {
       const user = await UserRepo.create(newUser)
       return user as User
     }
+
     return undefined
   } catch (e) {
     const error = e as Error
@@ -28,14 +29,42 @@ async function createOne(newUser: User): Promise<User | Error | undefined> {
   }
 }
 
-async function deleteUser(userId: string): boolean {
-  const result = await UserRepo.deleteUser(userId)
-  return result
+async function deleteUser(userId: string): Promise<number | Error> {
+  try {
+    const result = await UserRepo.deleteOne({ _id: userId }).exec()
+
+    if (result.deletedCount === 0) {
+      console.log(`No user with _id ${userId} found.`)
+    } else {
+      console.log(`User with _id ${userId} deleted successfully.`)
+    }
+
+    return result.deletedCount
+  } catch (e) {
+    const error = e as Error
+    return error
+  }
 }
 
-async function updateUser(userId: string, payload: UserUpdate): User | boolean {
-  const updatedUser = await UserRepo.updateUser(userId, payload)
-  return updatedUser
+async function updateUser(
+  userId: string,
+  payload: UserUpdate
+): Promise<boolean | Error> {
+  try {
+    const updatedUser = await UserRepo.updateOne(
+      { _id: userId },
+      payload
+    ).exec()
+
+    if (updatedUser.modifiedCount === 1) {
+      return true
+    } else {
+      throw new Error()
+    }
+  } catch (e) {
+    const error = e as Error
+    return error
+  }
 }
 
 export default {
