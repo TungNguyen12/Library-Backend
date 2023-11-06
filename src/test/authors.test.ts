8f9962772d8a7569daf7a8e7fe5a8ad60a903004
Tuan Nguyen <tuan.nguyen@tuni.fi>
2023-11-06 23:30:22 +0200

    modified testing scheme to match MongoDB usage

--- src/test/authors.test.ts
+++ src/test/authors.test.ts
@@ -1,49 +1,60 @@
 import { GenericContainer } from 'testcontainers'
-// import request from 'supertest'
-
+import request from 'supertest'
 import mongoose from 'mongoose'
 
-// import app from '../app.js'
-// import AuthorRepo from '../models/authorsModel.js'
-// import { type Author } from '../types/Author.js'
+import AuthorRepo from '../models/authorsModel.js'
+import app from '../app.js'
+import { authorsData } from './authorsData.js'
 
-describe('GET authors', () => {
+let mongoDbContainer: any
+
+beforeAll(async () => {
+  mongoDbContainer = await new GenericContainer('mongo')
+    .withExposedPorts(27017)
+    .start()
+
+  mongoose
+    .connect(`mongodb://localhost:${mongoDbContainer.getMappedPort(27017)}`)
+    .catch((err) => {
+      throw err
+    })
+})
+
+beforeEach(async () => {
+  for (const author of authorsData) {
+    const newAuthor = new AuthorRepo(author)
+    await newAuthor.save()
+  }
+})
+
+afterEach(async () => {
+  await AuthorRepo.deleteMany({})
+})
+
+afterAll(async () => {
+  await mongoose.disconnect()
+  await mongoDbContainer.stop()
+})
+
+describe('GET /authors', () => {
   test('should respond with status code 200 and all authors', async () => {
-    //   const response = await request(app).get('/api/v1/authors')
-    //   console.log('response:', response)
+    const response = await request(app).get('/api/v1/authors')
+    const authors = response.body
 
-    //   const authors = (await AuthorRepo.find().exec()) as Author[]
-    //   console.log('authors:', authors)
-
-    //   expect(response.statusCode).toBe(200)
-    //   expect(response.body.length).toBe(authors.length)
-
-    expect(true).toBe(true)
-  })
-
-  it('should work', async () => {
-    const mongoDbContainer = await new GenericContainer('mongo')
-      .withExposedPorts(27017)
-      .start()
-
-    mongoose
-      .connect(`mongodb://localhost:${mongoDbContainer.getMappedPort(27017)}`)
-      .catch((err) => {
-        throw err
-      })
-    await mongoose.disconnect()
-    await mongoDbContainer.stop()
+    expect(response.statusCode).toBe(200)
+    expect(authors.length).toBe(authorsData.length)
   })
 })
 
-// describe('GET author by id', () => {
-//   test('should respond with status code 404 and an error message when the author does not exist', async () => {
-//     const response = await request(app).get(
-//       '/api/v1/authors/65476d49bcb0ab378893f000'
-//     )
-//     expect(response.statusCode).toBe(404)
-//     expect(response.body).toMatchObject({
-//       message: 'Author not found.',
-//     })
-//   })
-// })
+describe('GET /author/authorId', () => {
+  test('should respond with status code 404 and an error message when the author does not exist', async () => {
+    const response = await request(app).get(
+      '/api/v1/authors/65476d49bcb0ab378893f000'
+    )
+
+    expect(response.statusCode).toBe(404)
+    expect(response.body).toMatchObject({
+      message: 'Author not found.',
+    })
+  })
+})
---
+++ src/test/authorsData.ts
@@ -0,0 +1,29 @@
+import { type Author } from '../types/Author.js'
+
+export const authorsData: Array<Partial<Author>> = [
+  {
+    firstName: 'Leo',
+    lastName: 'Tolstoy',
+    books: ['Anna Karenina'],
+  },
+  {
+    firstName: 'William',
+    lastName: 'Shakespeare',
+    books: ['Romeo and Juliet', 'Hamlet'],
+  },
+  {
+    firstName: 'Jane',
+    lastName: 'Austen',
+    books: ['Pride And Prejudice'],
+  },
+  {
+    firstName: 'George',
+    lastName: 'Orwell',
+    books: ['Animal Farm'],
+  },
+  {
+    firstName: 'Jules',
+    lastName: 'Verne',
+    books: ['Journey To The Centre Of The Earth'],
+  },
+]
