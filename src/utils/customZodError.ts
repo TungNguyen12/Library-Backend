66e5a2b2b7c7fcb6425664f025713d6f6a13dbc9
kien <mcfkcoding@gmail.com>
2023-11-09 05:41:48 +0200

    Fix type issue with issue.validation can return object

--- src/utils/customZodError.ts
+++ src/utils/customZodError.ts
@@ -32,10 +32,35 @@ const customErrorMap: ZodErrorMap = (issue, ctx) => {
       return {
         message: `Invalid date`,
       }
-    case ZodIssueCode.invalid_string:
-      return {
-        message: `Invalid ${String(issue.validation)}`,
+    case ZodIssueCode.invalid_string: {
+      switch (typeof issue.validation) {
+        case 'string':
+          return {
+            message: `Invalid ${issue.validation}`,
+          }
+        case 'object':
+          if ('includes' in issue.validation) {
+            if ('position' in issue.validation) {
+              return {
+                message: `${formattedField} ${issue.validation.includes} is not in ${issue.validation.position}`,
+              }
+            }
+            return {
+              message: `${formattedField} does not includes ${issue.validation.includes}`,
+            }
+          }
+          if ('startsWith' in issue.validation) {
+            return {
+              message: `${formattedField} does not start with ${issue.validation.startsWith}`,
+            }
+          }
+          return {
+            message: `${formattedField} does not end with ${issue.validation.endsWith}`,
+          }
+        default:
+          return { message: ctx.defaultError }
       }
+    }
     case ZodIssueCode.too_small: {
       if (issue.minimum > 1)
         return {
