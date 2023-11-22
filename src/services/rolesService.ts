53aeaf5096890754b2d9c456ef00a29aece7bd31
Phuoc Nguyen <phuoc94@hotmail.fi>
2023-11-22 04:51:15 +0200

    add test for role and fix role services

--- src/services/rolesService.ts
+++ src/services/rolesService.ts
@@ -1,72 +1,73 @@
 import mongoose, { type Types } from 'mongoose'
 
 import RoleRepo from '../models/rolesModel.js'
-import type { Role, RoleDTO, RoleWithPermissions } from '../types/User.js'
+import type {
+  RoleDTO,
+  RoleWithPermissionIds,
+  RoleWithPopulatedPermissions,
+} from '../types/Role.js'
 import { ApiError } from '../utils/ApiError.js'
 
-async function findAll(): Promise<Role[]> {
-  const roles = await RoleRepo.find().exec()
-  return roles as Role[]
+const rolesService = {
+  async findAll(): Promise<RoleWithPermissionIds[]> {
+    const roles = await RoleRepo.find().exec()
+    return roles as RoleWithPermissionIds[]
+  },
+
+  async findByTitle(title: string): Promise<RoleWithPermissionIds | null> {
+    const role = await RoleRepo.findOne({ title })
+    return role as RoleWithPermissionIds | null
+  },
+
+  async findById(
+    roleId: string | Types.ObjectId
+  ): Promise<RoleWithPermissionIds | null> {
+    const role = await RoleRepo.findById(roleId)
+    return role as RoleWithPermissionIds | null
+  },
+
+  async findByIdWithPermissions(
+    roleId: string | Types.ObjectId
+  ): Promise<RoleWithPopulatedPermissions | null> {
+    const id =
+      typeof roleId === 'string' ? new mongoose.Types.ObjectId(roleId) : roleId
+    const role = await RoleRepo.findById(id).populate('permissions')
+    return role as RoleWithPopulatedPermissions | null
+  },
+
+  async createRole(
+    newRole: RoleDTO
+  ): Promise<RoleWithPermissionIds | ApiError> {
+    const isAvailable = await RoleRepo.exists({ title: newRole.title })
+    if (isAvailable != null) {
+      return ApiError.conflict('Role already exists')
+    }
+
+    try {
+      const role = await RoleRepo.create(newRole)
+      return role as RoleWithPermissionIds
+    } catch (error) {
+      return ApiError.internal((error as Error).message)
+    }
+  },
+
+  async addPermission(
+    roleId: Types.ObjectId,
+    permissionId: Types.ObjectId
+  ): Promise<RoleWithPermissionIds | ApiError> {
+    const role = await RoleRepo.findById(roleId)
+    if (role == null) {
+      return ApiError.notFound('Role not found')
+    }
+
+    try {
+      role.permissions.push(permissionId)
+      await role.save()
+      return role as RoleWithPermissionIds
+    } catch (error) {
+      return ApiError.internal((error as Error).message)
+    }
+  },
 }
 
-// findByTitle
-async function findByTitle(title: string): Promise<Role | null> {
-  const role = await RoleRepo.findOne({ title })
-  return role as Role | null
-}
-
-async function findById(roleId: string): Promise<Role | null> {
-  const role = await RoleRepo.findOne({ _id: roleId })
-  return role as Role | null
-}
-
-async function findByIdWithPermissions(
-  roleId: string | Types.ObjectId
-): Promise<RoleWithPermissions | null> {
-  const id =
-    typeof roleId === 'string' ? new mongoose.Types.ObjectId(roleId) : roleId
-  const role = await RoleRepo.findOne({ _id: id }).populate('permissions')
-  return role as RoleWithPermissions | null
-}
-
-async function createRole(newRole: RoleDTO): Promise<Role | ApiError> {
-  const isAvailable = await RoleRepo.exists({ title: newRole.title })
-  if (isAvailable !== null) {
-    return ApiError.conflict('Role already exists')
-  }
-
-  try {
-    const role = await RoleRepo.create(newRole)
-    return role as Role
-  } catch (error) {
-    const e = error as Error
-    return ApiError.internal(e.message)
-  }
-}
-
-async function addPermission(
-  roleId: Types.ObjectId,
-  permissionId: Types.ObjectId
-): Promise<Role | ApiError> {
-  const role = await RoleRepo.findOne({ _id: roleId })
-  if (role === null) {
-    return ApiError.notFound('Role not found')
-  }
-  try {
-    role.permissions.push(permissionId)
-    await role.save()
-    return role as Role
-  } catch (error) {
-    const e = error as Error
-    return ApiError.internal(e.message)
-  }
-}
-
-export default {
-  findAll,
-  findByTitle,
-  findById,
-  findByIdWithPermissions,
-  createRole,
-  addPermission,
-}
+export default rolesService
