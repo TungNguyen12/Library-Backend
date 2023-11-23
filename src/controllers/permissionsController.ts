e3b829fabfe2a627341eb9db884da4c38b52081e
Phuoc Nguyen <phuoc94@hotmail.fi>
2023-11-23 23:01:56 +0200

    fix extendResponse

--- src/controllers/permissionsController.ts
+++ src/controllers/permissionsController.ts
@@ -2,6 +2,7 @@ import type { NextFunction, Request, Response } from 'express'
 
 import PermissionsService from '../services/permissionsService.js'
 import { ApiError } from '../utils/ApiError.js'
+import { type ExtendedResponse } from '../utils/responseExtensions.js'
 
 const permissionsController = {
   async findAllPermissions(_: Request, res: Response): Promise<void> {
@@ -11,7 +12,7 @@ const permissionsController = {
 
   async findOnePermission(
     req: Request,
-    res: Response,
+    res: ExtendedResponse,
     next: NextFunction
   ): Promise<void> {
     const permissionId = req.params.permissionId
