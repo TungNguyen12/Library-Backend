b63bf75fabd2cd50f07244a0541d45911196dc5e
Phuoc Nguyen <phuoc94@hotmail.fi>
2023-11-23 22:13:26 +0200

    protect routes

--- src/routes/permissionsRoutes.ts
+++ src/routes/permissionsRoutes.ts
@@ -2,6 +2,7 @@ import express from 'express'
 
 import PermissionsController from '../controllers/permissionsController.js'
 import { checkAuth } from '../middlewares/checkAuth.js'
+import { checkPermission } from '../middlewares/checkPermission.js'
 import {
   validateCreatePermission,
   validateUpdatePermission,
@@ -9,23 +10,36 @@ import {
 
 const router = express.Router()
 
-router.get('/', checkAuth, PermissionsController.findAllPermissions)
-router.get('/:permissionId', checkAuth, PermissionsController.findOnePermission)
+router.get(
+  '/',
+  checkAuth,
+  checkPermission('PERMISSIONS_READ'),
+  PermissionsController.findAllPermissions
+)
+router.get(
+  '/:permissionId',
+  checkAuth,
+  checkPermission('PERMISSIONS_READ'),
+  PermissionsController.findOnePermission
+)
 
 router.post(
   '/',
   checkAuth,
+  checkPermission('PERMISSIONS_CREATE'),
   validateCreatePermission,
   PermissionsController.createNewPermission
 )
 router.delete(
   '/:permissionId',
   checkAuth,
+  checkPermission('PERMISSIONS_DELETE'),
   PermissionsController.deletePermission
 )
 router.put(
   '/:permissionId',
   checkAuth,
+  checkPermission('PERMISSIONS_UPDATE'),
   validateUpdatePermission,
   PermissionsController.updatePermission
 )
