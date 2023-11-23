e3902725bc407044ec9d3bb8a5fc75ef487aadf0
Tuan Nguyen <tuan.nguyen@tuni.fi>
2023-11-23 22:09:38 +0200

    added tests for AuthorsService

--- src/test/mockData/authorsData.ts
+++ src/test/mockData/authorsData.ts
@@ -12,8 +12,8 @@ export const authorsData: Array<
     firstName: 'Leo',
     lastName: 'Tolstoy',
     books: [
-      new ObjectId('6546a7febac08f6bd30c0506'),
-      new ObjectId('6546a7febac08f6bd30c0507'),
+      new ObjectId('655d13daf50dd1ceca878b43'),
+      new ObjectId('655ec8104202fd2aa0055472'),
     ],
   },
   {
