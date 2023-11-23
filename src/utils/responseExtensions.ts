257ea5047f6443a05343ac63158aabfb0cd57c96
Phuoc Nguyen <phuoc94@hotmail.fi>
2023-11-23 21:57:09 +0200

    Add response extensions to Express

---
+++ src/utils/responseExtensions.ts
@@ -0,0 +1,25 @@
+import { type Express, type Response } from 'express'
+
+declare module 'express-serve-static-core' {
+  interface Response {
+    ok: (data: any) => Response
+    created: (data: any) => Response
+    deleted: () => Response
+  }
+}
+
+export function extendResponse(express: Express): void {
+  const res = express.response
+
+  res.ok = function (data: any): Response {
+    return this.status(200).json(data)
+  }
+
+  res.created = function (data: any): Response {
+    return this.status(201).json(data)
+  }
+
+  res.deleted = function (): Response {
+    return this.status(204).send()
+  }
+}
