7a8deedf8bc6b36e97ff0049ed8a321c4972ec3a
Phuoc Nguyen <phuoc94@hotmail.fi>
2023-11-23 21:31:42 +0200

    Refactor permissionsData structure

--- src/test/mockData/permissionsData.ts
+++ src/test/mockData/permissionsData.ts
@@ -4,11 +4,14 @@ import { type BasicPermission } from '../../types/Permission.js'
 
 const ObjectId = mongoose.Types.ObjectId
 
-export const permissionsData: Array<
-  Partial<BasicPermission & { _id: mongoose.Types.ObjectId }>
-> = [
+export const permissionsData: BasicPermission[] = [
   {
-    _id: new ObjectId('6546a7febac08f6bd30c0505'),
+    id: new ObjectId('6546a7febac08f6bd30c0505'),
     action: 'USERS_CREATE',
   },
 ]
+
+export const convertedPermissionsData = permissionsData.map((permission) => ({
+  _id: permission.id,
+  action: permission.action,
+}))
