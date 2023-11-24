679c6a68a9dd2f07d698aec5c360041a41f75b75
kien <mcfkcoding@gmail.com>
2023-11-24 17:11:54 +0200

    Add book service test

--- src/test/mockData/booksData.ts
+++ src/test/mockData/booksData.ts
@@ -37,6 +37,17 @@ export const booksData: Book[] = [
   },
 ]
 
+export const convertedBookData = booksData.map((book) => ({
+  _id: book.id,
+  ISBN: book.ISBN,
+  title: book.title,
+  edition: book.edition,
+  category: book.category,
+  description: book.description,
+  publisher: book.publisher,
+  author: book.author,
+}))
+
 export const BookCopiesData = [
   {
     _id: new ObjectId('655d145b404afab97bd851da'),
@@ -44,8 +55,24 @@ export const BookCopiesData = [
     is_Available: true,
   },
   {
-    _id: new ObjectId('655d145b404afab97bd851da'),
+    _id: new ObjectId('655d145b404afab97bd851e0'),
     book_id: new ObjectId('655d13daf50dd1ceca878b43'),
     is_Available: false,
+  },
+]
+
+export const BorrowedBookData = [
+  {
+    _id: new ObjectId('655e9ced4202fd2aa005546d'),
+    copy_id: new ObjectId('655d145b404afab97bd851da'),
+    user_id: new ObjectId('655d1091fba90fb470aa806f'),
+    borrowed_Date: '2023-11-22T22:15:21.852Z',
+    returned_Date: '2023-11-22T22:16:21.852Z',
+  },
+  {
+    _id: new ObjectId('655e7d79a0e52b844d17753d'),
+    copy_id: new ObjectId('655d145b404afab97bd851e0'),
+    user_id: new ObjectId('655d1091fba90fb470aa806f'),
+    borrowed_Date: '2023-11-22T22:15:21.852Z',
   },
 ]
