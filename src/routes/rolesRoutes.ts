e4f9bb197dc24d361502a15671b9511ae69e730b
Phuoc Nguyen <phuoc94@hotmail.fi>
2023-11-24 15:02:24 +0200

    add swagger documentation

--- src/routes/rolesRoutes.ts
+++ src/routes/rolesRoutes.ts
@@ -7,6 +7,17 @@ import { validateCreateRole } from '../middlewares/roleValidate.js'
 
 const router = express.Router()
 
+/**
+ * @swagger
+ * /api/v1/roles:
+ *   get:
+ *     summary: Get all roles
+ *     security:
+ *      - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Successful response
+ */
 router.get(
   '/',
   checkAuth,
@@ -14,6 +25,27 @@ router.get(
   RolesController.findAllRoles
 )
 
+/**
+ * @swagger
+ * /api/v1/roles:
+ *   post:
+ *     summary: Create a new role
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *                 description: The role title
+ *                 example: ADMIN
+ *     responses:
+ *       201:
+ *         description: Successful response
+ */
 router.post(
   '/',
   checkAuth,
@@ -22,6 +54,33 @@ router.post(
   RolesController.createNewRole
 )
 
+/**
+ * @swagger
+ * /api/v1/roles/{roleId}/addPermission:
+ *   post:
+ *     summary: add a permission to a role
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: roleId
+ *         required: true
+ *         description: The role id
+ *         example: 655461aee5407a09ec63d104
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               permissionId:
+ *                 type: string
+ *                 description: The permission id
+ *                 example: 65609a888593268849a68eb8
+ *     responses:
+ *       201:
+ *         description: Successful response
+ */
 router.post(
   '/:roleId/addPermission',
   checkAuth,
