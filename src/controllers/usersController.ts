9fdc9031d209baf4bf329747492f2fef02da5791
TungNguyen12 <nvtung382@gmail.com>
2023-12-02 12:42:24 +0200

    Login and get uses profile

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -28,6 +28,23 @@ export async function findOneUser(
   }
   res.json(user)
 }
+export async function findByEmail(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  const userEmail = req.params.email
+  const user = await UsersService.findByEmail(userEmail)
+
+  if (user === null) {
+    next(ApiError.notFound('User not found'))
+    return
+  } else if (user instanceof Error) {
+    next(ApiError.badRequest('Bad request.', user.message))
+    return
+  }
+  res.json(user)
+}
 
 export async function createNewUser(
   req: Request,
@@ -42,7 +59,7 @@ export async function createNewUser(
     )
     return
   } else if (!(user instanceof Error)) {
-    const roleId = new mongoose.Types.ObjectId('655461b3e5407a09ec63d108')
+    const roleId = new mongoose.Types.ObjectId('6569d985a7c5b908fba44d07')
     await UserRolesService.addRoleToUser({
       user_id: user.id,
       role_id: roleId,
@@ -92,6 +109,7 @@ export async function updateUser(
 
 export default {
   findOneUser,
+  findByEmail,
   findAllUsers,
   createNewUser,
   deleteUser,
