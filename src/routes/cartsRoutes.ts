7be5479009c1c16aac97d3e8f0dbf6149edeb02b
Tuan Nguyen <tuannguyen221101@gmail.com>
2023-12-04 23:56:55 +0200

    added middlewares to protect cartRoutes

--- src/routes/cartsRoutes.ts
+++ src/routes/cartsRoutes.ts
@@ -1,15 +1,52 @@
 import express from 'express'
 
 import CartsController from '../controllers/cartsController.js'
+import { checkAuth } from '../middlewares/checkAuth.js'
+import { checkPermission } from '../middlewares/checkPermission.js'
 
 const router = express.Router()
 
-router.get('/', CartsController.getAllCarts)
-router.get('/items', CartsController.getAllCartItems)
-router.get('/:userId', CartsController.getCartByUserId)
-router.put('/:userId', CartsController.addToCart)
-router.post('/:userId/checkout', CartsController.checkout)
-router.delete('/:userId/:bookId', CartsController.removeFromCart)
-router.delete('/:userId', CartsController.deleteCart)
+router.get(
+  '/',
+  checkAuth,
+  checkPermission('CART_READ'),
+  CartsController.getAllCarts
+)
+router.get(
+  '/items',
+  checkAuth,
+  checkPermission('CART_READ'),
+  CartsController.getAllCartItems
+)
+router.get(
+  '/:userId',
+  checkAuth,
+  checkPermission('CART_READ_ONE'),
+  CartsController.getCartByUserId
+)
+router.put(
+  '/:userId',
+  checkAuth,
+  checkPermission('CART_UPDATE_ONE'),
+  CartsController.addToCart
+)
+router.post(
+  '/:userId/checkout',
+  checkAuth,
+  checkPermission('CART_UPDATE_ONE'),
+  CartsController.checkout
+)
+router.delete(
+  '/:userId/:bookId',
+  checkAuth,
+  checkPermission('CART_DELETE_ONE'),
+  CartsController.removeFromCart
+)
+router.delete(
+  '/:userId',
+  checkAuth,
+  checkPermission('CART_DELETE'),
+  CartsController.deleteCart
+)
 
 export default router
